e83d6b8eac493306e0b09c177f1b359f5408cb8f
Tamir Sida <tamir.sida@post.runi.ac.il>
2025-09-13 16:40:16 +0300

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,11 +77,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Favicon and Icons */}
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        {/* Web App Meta */}
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#2563eb" />
         <meta name="msapplication-TileColor" content="#2563eb" />
