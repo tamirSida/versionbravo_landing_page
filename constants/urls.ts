c9616846b13264f4726230c68e191abcf68932ef
Tamir Sida <tamir.sida@post.runi.ac.il>
2025-12-07 00:57:01 +0200

    pre size

--- constants/urls.ts
+++ constants/urls.ts
@@ -1,4 +1,9 @@
 export const APPLICATION_URLS = {
-  ALPHA_BET: 'https://alphabet.vbv.vc/apply',
-  ACCELERATOR: 'https://accelerator.thevetted.vc/apply'
+  ALPHA_BET: 'https://application.alphabet.vbv.vc',
+  ACCELERATOR: 'https://application.thevetted.vc/'
+} as const;
+
+export const WEBSITE_URLS = {
+  ALPHA_BET: 'https://alphabet.vbv.vc',
+  ACCELERATOR: 'https://accelerator.thevetted.vc/'
 } as const;
